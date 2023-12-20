5753780cef66e6c40d865ffe9b529e81db658809
IrinaP3110 <irina.3110.pavlovic@gmail.com>
2023-12-20 16:28:15 +0100

    Promenjen naziv kolone name u username u tabeli users koriscenjem migracije

--- management-api/database/migrations/2014_10_12_000000_create_users_table.php
+++ management-api/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username')->unique();
+            $table->string('name')->unique();
             $table->string('firstname');
             $table->string('lastname');
             $table->string('position');
