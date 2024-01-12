ac406265f9a3d93419b8f6490cc780ed21c3e9b5
JelisavetaR <radulovicjelisaveta5@gmail.com>
2024-01-12 20:34:23 +0100

    Finalni kod

--- management-api/routes/api.php
+++ management-api/routes/api.php
@@ -37,7 +37,8 @@ Route::post('/register',[AuthController::class,'register']);
 Route::post('/login',[AuthController::class,'login'])->name('getLogin');
 Route::post('/forgotpassword',[AuthController::class,'forgotpassword']);
 Route::post('/resetpassword',[AuthController::class,'resetpassword']);
-
+Route::post('/updateuser/{id}',[UserController::class, 'update']);
+Route::post('/adduser',[UserController::class, 'store']);
 Route::group(['middleware'=>['auth:sanctum']], function(){
     Route::get('/profile',function(Request $request){
         return auth()->user();
@@ -46,77 +47,94 @@ Route::group(['middleware'=>['auth:sanctum']], function(){
     });
     Route::resource('task',TaskController::class)->only('destroy','store','update');
     Route::post('/logout',[AuthController::class,'logout']);
-});
 
-
-Route::group(['middleware' => ['auth:sanctum', 'admin']], function () {
+    //ispod su zajednicke
     Route::get('/projects/{id}',[ProjectController::class, 'show']);
     Route::get('/tasks/filter/{status}', [TaskController::class, 'filterByStatus']);
     Route::get('/projects/filter/{priority}', [ProjectController::class, 'filterByPriority']);
     Route::get('/users/filter/{role}', [UserController::class, 'filterByRole']);
-
     Route::get('/projects',[ProjectController::class, 'index']);
-    Route::post('/addprojects',[ProjectController::class, 'store'])->name('add.project');
     Route::post('/updateprojects/{id}',[ProjectController::class, 'update']);
-    Route::delete('/deleteprojects/{id}',[ProjectController::class, 'destroy']);
+    //
     Route::resource('tasks',TaskController::class);
     Route::resource('users',UserController::class);
     Route::resource('categories',CategoryController::class);
     Route::get('/projects/{id}/tasks',[ProjectTaskController::class,'index'])->name('projects.tasks.index');
     Route::resource('projects.tasks',ProjectTaskController::class)->only(['index']);
     Route::get('/categories/{id}',[CategoryController::class, 'show']);
-    Route::post('/addcategory',[CategoryController::class, 'store']);
     Route::post('/updatecategory/{id}',[CategoryController::class, 'update']);
+    Route::post('/updatetask/{id}',[TaskController::class, 'update']);
+});
+
+
+Route::group(['middleware' => ['auth:sanctum', 'admin']], function () {
+    //Route::get('/projects/{id}',[ProjectController::class, 'show']);
+    //Route::get('/tasks/filter/{status}', [TaskController::class, 'filterByStatus']);
+    //Route::get('/projects/filter/{priority}', [ProjectController::class, 'filterByPriority']);
+    //Route::get('/users/filter/{role}', [UserController::class, 'filterByRole']);
+
+    //Route::get('/projects',[ProjectController::class, 'index']);
+    Route::post('/addprojects',[ProjectController::class, 'store'])->name('add.project');
+    //Route::post('/updateprojects/{id}',[ProjectController::class, 'update']);
+    Route::delete('/deleteprojects/{id}',[ProjectController::class, 'destroy']);
+   // Route::resource('tasks',TaskController::class);
+    //Route::resource('users',UserController::class);
+    //Route::resource('categories',CategoryController::class);
+   // Route::get('/projects/{id}/tasks',[ProjectTaskController::class,'index'])->name('projects.tasks.index');
+    //Route::resource('projects.tasks',ProjectTaskController::class)->only(['index']);
+    //Route::get('/categories/{id}',[CategoryController::class, 'show']);
+    Route::post('/addcategory',[CategoryController::class, 'store']);
+    //Route::post('/updatecategory/{id}',[CategoryController::class, 'update']);
     Route::delete('/deletecategory/{id}',[CategoryController::class, 'destroy']);
     Route::post('/addtask',[TaskController::class, 'store']);
-    Route::post('/updatetask/{id}',[TaskController::class, 'update']);
-    Route::post('/adduser',[UserController::class, 'store']);
-    Route::post('/updateuser/{id}',[UserController::class, 'update']);
+    //Route::post('/updatetask/{id}',[TaskController::class, 'update']);
+    //Route::post('/adduser',[UserController::class, 'store']);
+    //Route::post('/updateuser/{id}',[UserController::class, 'update']);
     Route::delete('/deleteuser/{id}',[UserController::class, 'destroy']);
 
 });
 
  Route::group(['middleware' => ['auth:sanctum', 'vip']], function () {
+    //Route::get('/projects/{id}',[ProjectController::class, 'show']);
+    //Route::get('/tasks/filter/{status}', [TaskController::class, 'filterByStatus']);
     Route::get('/projects/{id}',[ProjectController::class, 'show']);
     Route::get('/tasks/filter/{status}', [TaskController::class, 'filterByStatus']);
-    Route::get('/projects/{id}',[ProjectController::class, 'show']);
-    Route::get('/tasks/filter/{status}', [TaskController::class, 'filterByStatus']);
-    Route::get('/projects/filter/{priority}', [ProjectController::class, 'filterByPriority']);
-    Route::get('/users/filter/{role}', [UserController::class, 'filterByRole']);
-    Route::get('/projects',[ProjectController::class, 'index']);
+    //Route::get('/projects/filter/{priority}', [ProjectController::class, 'filterByPriority']);
+    //Route::get('/users/filter/{role}', [UserController::class, 'filterByRole']);
+    //Route::get('/projects',[ProjectController::class, 'index']);
     Route::post('/addprojects',[ProjectController::class, 'store'])->name('add.project');
-    Route::post('/updateprojects/{id}',[ProjectController::class, 'update']);
-    Route::resource('tasks',TaskController::class);
-    Route::resource('users',UserController::class);
+    //Route::post('/updateprojects/{id}',[ProjectController::class, 'update']);
+   // Route::resource('tasks',TaskController::class);
+    /*Route::resource('users',UserController::class);
     Route::resource('categories',CategoryController::class);
     Route::get('/projects/{id}/tasks',[ProjectTaskController::class,'index'])->name('projects.tasks.index');
     Route::resource('projects.tasks',ProjectTaskController::class)->only(['index']);
-    Route::get('/categories/{id}',[CategoryController::class, 'show']);
+    Route::get('/categories/{id}',[CategoryController::class, 'show']);*/
     Route::post('/addcategory',[CategoryController::class, 'store']);
-    Route::post('/updatecategory/{id}',[CategoryController::class, 'update']);
+    //Route::post('/updatecategory/{id}',[CategoryController::class, 'update']);
     Route::post('/addtask',[TaskController::class, 'store']);
-    Route::post('/updatetask/{id}',[TaskController::class, 'update']);
+    //Route::post('/updatetask/{id}',[TaskController::class, 'update']);
 
     
 });
 
 Route::group(['middleware' => ['auth:sanctum', 'korisnik']], function () {
-    Route::get('/projects/{id}',[ProjectController::class, 'show']);
-    Route::get('/tasks/filter/{status}', [TaskController::class, 'filterByStatus']);
-    Route::get('/projects/{id}',[ProjectController::class, 'show']);
-    Route::get('/tasks/filter/{status}', [TaskController::class, 'filterByStatus']);
-    Route::get('/projects/filter/{priority}', [ProjectController::class, 'filterByPriority']);
-    Route::get('/users/filter/{role}', [UserController::class, 'filterByRole']);
-    Route::get('/projects',[ProjectController::class, 'index']);
-    Route::post('/updateprojects/{id}',[ProjectController::class, 'update']);
-    Route::resource('tasks',TaskController::class);
+    //Route::get('/projects/{id}',[ProjectController::class, 'show']);
+    //Route::get('/tasks/filter/{status}', [TaskController::class, 'filterByStatus']);
+    //Route::get('/projects/{id}',[ProjectController::class, 'show']);
+    //Route::get('/tasks/filter/{status}', [TaskController::class, 'filterByStatus']);
+    //Route::get('/projects/filter/{priority}', [ProjectController::class, 'filterByPriority']);
+    //Route::get('/users/filter/{role}', [UserController::class, 'filterByRole']);
+    //Route::get('/projects',[ProjectController::class, 'index']);
+    //Route::post('/updateprojects/{id}',[ProjectController::class, 'update']);
+   /* Route::resource('tasks',TaskController::class);
     Route::resource('users',UserController::class);
     Route::resource('categories',CategoryController::class);
     Route::get('/projects/{id}/tasks',[ProjectTaskController::class,'index'])->name('projects.tasks.index');
     Route::resource('projects.tasks',ProjectTaskController::class)->only(['index']);
     Route::get('/categories/{id}',[CategoryController::class, 'show']);
     Route::post('/updatecategory/{id}',[CategoryController::class, 'update']);
-    Route::post('/updatetask/{id}',[TaskController::class, 'update']);
+    Route::post('/updatetask/{id}',[TaskController::class, 'update']);*/
 
     
 });
