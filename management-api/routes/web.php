1a17abf0ee526a9b845e7ddcd4578b3c2a570d88
JelisavetaR <radulovicjelisaveta5@gmail.com>
2024-02-05 17:18:20 +0100

    web rute za javni servis

--- management-api/routes/web.php
+++ management-api/routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/twitter', function () {
+    return redirect('https://twitter.com/mattermost');
+});
