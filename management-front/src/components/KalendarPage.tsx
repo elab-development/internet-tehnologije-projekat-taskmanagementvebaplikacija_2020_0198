056eb5122ef5e1147d1d52b359555383b33362dc
JelisavetaR <radulovicjelisaveta5@gmail.com>
2024-02-11 20:57:59 +0100

    Finalni ispravljen

--- management-front/src/components/KalendarPage.tsx
+++ management-front/src/components/KalendarPage.tsx
@@ -1,53 +1,51 @@
 import React, { useState, useEffect } from 'react';
-import { useGet } from '../hooks';
+import { useUserContext } from '../context/UserContext'
 import { Task } from '../model';
+import { User } from '../model';
+import { useGet } from '../hooks'
 
-export default function KalendarPage() {
+export default function Calendar() {
   const [tasks, setTasks] = useState<Task[]>([]);
-  const { data: tasksData } = useGet<Task[]>('/api/tasks');
+  const { user } = useUserContext(); // Dobijamo trenutnog korisnika
+
+   // Dobijamo zadatke sa servera
+  const { data: tasksData } = useGet<Task>('/api/tasks');
   
-  // Ako je `tasksData` niz nizova zadataka
   useEffect(() => {
-    if (tasksData && tasksData.length > 0) {
-      const flattenedTasks: Task[] = tasksData.flatMap(taskArray => taskArray);
-      setTasks(flattenedTasks);
+    if (tasksData && tasksData.length > 0 && user ) {
+      // Filtriramo zadatke samo za trenutnog korisnika
+      const userTasks = tasksData.filter(task => task.user.username === user.username);
+      setTasks(userTasks);
     }
-  }, [tasksData]);
+  }, [tasksData, user]);
   
   return (
     <div className="container mt-4">
       <div className="row">
         <div className="col">
           <div className="column active-column" style={{ backgroundColor: '#F4C2C2' }}>
-            <h2 style={{ color: '#E15656' }}>All Tasks</h2>
-            {tasks.map((task) => (
-              <div key={task.id} className="task" style={{ backgroundColor: '#f0f0f0' }}>
-                <p>Name: {task.name}</p>
-                <p>Project Name: {getProjectName(task)}</p>
-                <p>Start Date: {getStartDate(task)}</p>
-                <p>End Date: {getEndDate(task)}</p>
-                <p>User Name: {getUserName(task)}</p>
-              </div>
-            ))}
+            <h2 style={{ color: '#E15656' }}>User's Tasks</h2>
+            <table>
+              <thead>
+                <tr>
+                  <th>Project Name</th>
+                  <th>Start Date</th>
+                  <th>End Date</th>
+                </tr>
+              </thead>
+              <tbody>
+                {tasks.map((task) => (
+                  <tr key={task.id} className="task" style={{ backgroundColor: '#f0f0f0' }}>
+                    <td>{task.project?.name}</td>
+                    <td>{task.project?.start_date}</td>
+                    <td>{task.project?.end_date}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
     </div>
   );
-}
-
-function getProjectName(task: Task): string {
-  return task.project?.name;
-}
-
-function getStartDate(task: Task): string {
-  return task.project?.start_date;
-}
-
-function getEndDate(task: Task): string {
-  return task.project?.end_date;
-}
-
-function getUserName(task: Task): string {
-  return task?.user?.username;
 }
